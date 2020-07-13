32e953870d7270c7d39e074860670272cd57c1a6
Frewen.Wong <frewen1225@gmail.com>
2020-07-13 10:37:53 +0800

    [MDF] learn hash

--- CollectionSamples/src/com/frewen/collection/demo/map/HashMapDemo.java
+++ CollectionSamples/src/com/frewen/collection/demo/map/HashMapDemo.java
@@ -1,5 +1,6 @@
 package com.frewen.collection.demo.map;
 
+import java.util.Arrays;
 import java.util.HashMap;
 public class HashMapDemo {
 
@@ -7,9 +8,10 @@ public class HashMapDemo {
 
     public static void main(String[] args) {
 
+
+        System.out.println("============计算hash值================");
+
         HashMap<Integer, String> map = new HashMap<>();
-
-
         //计算Key值的Hash值
         //int hash = hash(new Integer(15));
         int hash = hash(new String("1245"));
@@ -40,9 +42,10 @@ public class HashMapDemo {
     static final int hash(Object key) {
         int h;
         System.out.println("h = key.hashCode()==" + (h = key.hashCode()) + ",二进制：" + Integer.toBinaryString(h));
-
+        // 将Key的hashCode的值向右移动16位，前面用0补齐。
         System.out.println("h >>> 16==" + (h >>> 16) + "，二进制：" + Integer.toBinaryString(h >>> 16));
-
+        // 按位异或(^)：参加运算的两个数，换算为二进制(0、1)后，进行异或运算。只有当相应位上的数字不相同时，该为才取1，若相同，即为0。
+        /// 所以其实我们可以得到一个结论，任何一个数和其自身按位异或都为0
         System.out.println("(h = key.hashCode()) ^ (h >>> 16) ==二进制：" + Integer.toBinaryString(((h = key.hashCode()) ^ (h >>> 16))));
         return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
     }
